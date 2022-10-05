703c041ea1ba9537d500400a38800826b182f05d
yashdondal <yashdondal21@gmail.com>
2022-10-05 17:42:13 +0530

    Welcome To LIne Comparision

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Line_Comparision
+ {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Line Comparison Computation");
+            //LengthOfLine.Length();
+            //LengthEqual.Length();
+            Line.Length();
+        }
+    }
+ }
